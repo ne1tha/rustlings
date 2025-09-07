b2eb26047d1e6ab4466e0a850f2e7f3ea07af1c4
进取之魂 <14864493+neltha@user.noreply.gitee.com>
2025-09-07 17:16:39 +0800

    fix4

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,15 +32,12 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
-        if !basket.contains_key(&fruit) {
-            basket.insert(fruit, 1); // Insert one of each new fruit
-        } else {
-            // If the fruit is already in the basket, we can skip it.
-            continue;
-        }
+        basket.entry(fruit).or_insert(1);
+        // if !basket.contains_key(&fruit) {
+        //     basket.insert(fruit, 1);
+        // }
     }
 }
-
 
 fn main() {
     // You can optionally experiment here.
@@ -49,6 +46,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::iter::FromIterator;
 
     // Don't modify this function!
     fn get_fruit_basket() -> HashMap<Fruit, u32> {
